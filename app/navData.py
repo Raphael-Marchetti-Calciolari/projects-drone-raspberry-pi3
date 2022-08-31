f8d58cd9a54ef8adf9d3c96bed55ebf55b56b584
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-08-31 16:39:33 -0300

    Created sample app for reading navdata

---
+++ app/navData.py
@@ -0,0 +1,4 @@
+from pyardrone import ARDrone
+
+pyardrone.ARDrone(*, host='192.168.1.1', at_port=5556, navdata_port=5554,
+video_port=5555, watchdog_interval=0.5, timeout=0.01, bind=True, connect=True)
