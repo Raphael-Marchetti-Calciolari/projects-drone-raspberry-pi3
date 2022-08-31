12ed325cc724fffe8223d9f970781fd016e3d8e0
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-08-31 18:15:17 -0300

    Update navData.py

--- app/navData.py
+++ app/navData.py
@@ -1,4 +1,5 @@
-from pyardrone import ARDrone
+import ardrone
 
-pyardrone.ARDrone(*, host='192.168.1.1', at_port=5556, navdata_port=5554,
-video_port=5555, watchdog_interval=0.5, timeout=0.01, bind=True, connect=True)
+drone = ardrone.ARDrone()
+
+print(drone.navdata)
