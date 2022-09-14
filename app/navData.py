33ad284ca6da04b3da5502c8ac6c8e4b93f8a3a3
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-09-14 16:19:04 -0300

    Martin

--- app/navData.py
+++ app/navData.py
@@ -1,5 +1,12 @@
-import ardrone
+import ardrone # https://github.com/lilyinstarlight/python-ardrone
 
 drone = ardrone.ARDrone()
 
+drone.takeoff()
+drone.land()
+
 print(drone.navdata)
+
+drone.image.show()
+
+drone.halt()
