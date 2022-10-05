ad103c37f0230f8d2e0df844128babd59cb8583d
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-10-05 16:28:39 -0300

    fix hold down key

--- app/src/sample.py
+++ app/src/sample.py
@@ -1,6 +1,8 @@
 import pygame
 from pyardrone import ARDrone
 
+
+
 def sample(drone: ARDrone):
     pygame.init()
     W, H = 320, 240
@@ -10,75 +12,63 @@ def sample(drone: ARDrone):
     running = True
     while running:
         for event in pygame.event.get():
+            print(f"Event: {event}")
             if event.type == pygame.QUIT:
                 running = False 
             elif event.type == pygame.KEYUP:
                 drone.hover()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    # drone.reset()
-                    print('Drone reseted')
-                    running = False
-                # takeoff / land
-                elif event.key == pygame.K_RETURN:
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
                     drone.takeoff()
-                elif event.key == pygame.K_SPACE:
+
+                elif event.key == pygame.K_UP:
+                    print("CIMA")
+                    drone.move(up=speed)
+                elif event.key == pygame.K_DOWN:
+                    print("BAIXO")
+                    drone.move(down=speed)
+
+
+            elif event.type == pygame.TEXTINPUT:
+
+                print(f"Key Pressed! -> {event.text}")
+
+
+                if event.text == chr(pygame.K_SPACE):
                     drone.land()
                 # emergency
-                elif event.key == pygame.K_BACKSPACE:
+                elif event.text == chr(pygame.K_BACKSPACE):
                     # drone.reset()
                     print('Drone reseted')
                 # forward / backward
-                elif event.key == pygame.K_w:
+                elif event.text == chr(pygame.K_w):
+                    print("FRENTE")
                     drone.move(forward=speed)
-                elif event.key == pygame.K_s:
+                elif event.text == chr(pygame.K_s):
+                    print("TRAS")
                     drone.move(backward=speed)
                 # left / right
-                elif event.key == pygame.K_a:
+                elif event.text == chr(pygame.K_a):
+                    print("ESQUERDA")
                     drone.move(left=speed)
-                elif event.key == pygame.K_d:
+                elif event.text == chr(pygame.K_d):
+                    print("DIREITA")
                     drone.move(right=speed)
                 # up / down
-                elif event.key == pygame.K_UP:
-                    drone.move(up=speed)
-                elif event.key == pygame.K_DOWN:
-                    drone.move(down=speed)
+
                 # turn left / turn right
-                elif event.key == pygame.K_q:
+                elif event.text == chr(pygame.K_q):
+                    print("GIRAR ANTI-HORARIO")
                     drone.move(ccw=speed)
-                elif event.key == pygame.K_e:
+                elif event.text == chr(pygame.K_e):
+                    print("GIRAR HORARIO")
                     drone.move(cw=speed)
                 # speed
-                elif event.key == pygame.K_1:
-                    speed = 0.1
+                elif event.text in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
+                    speed = int(event.text)/10 if event.text != "0" else 1
                     print(f'Current speed: {speed}')
-                elif event.key == pygame.K_2:
-                    speed = 0.2
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_3:
-                    speed = 0.3
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_4:
-                    speed = 0.4
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_5:
-                    speed = 0.5
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_6:
-                    speed = 0.6
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_7:
-                    speed = 0.7
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_8:
-                    speed = 0.8
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_9:
-                    speed = 0.9
-                    print(f'Current speed: {speed}')
-                elif event.key == pygame.K_0:
-                    speed = 1.0
-                    print(f'Current speed: {speed}')
+
 
         try:
             surface = pygame.image.fromstring(drone.image, (W, H), 'RGB')
